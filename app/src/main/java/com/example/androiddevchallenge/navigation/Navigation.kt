e55031b1679de4766b3088e6cbc7c8e809de40dd
João Gabriel <gsantana152@gmail.com>
2021-03-02 01:27:35 -0300

    test

--- app/src/main/java/com/example/androiddevchallenge/navigation/Navigation.kt
+++ app/src/main/java/com/example/androiddevchallenge/navigation/Navigation.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.androiddevchallenge.navigation
 
 import androidx.navigation.NavHostController
@@ -19,6 +35,4 @@ class Actions(navController: NavHostController) {
             navController.navigate("$CAT_DETAIL_SCREEN/$id")
             println("$CAT_DETAIL_SCREEN/{$id}")
         }
-
-    val navigateBack: () -> Unit = { navController.popBackStack() }
 }
--- app/src/main/java/com/example/androiddevchallenge/ui/CatsFactory.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/CatsFactory.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright 2021 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.androiddevchallenge.ui
 
 import com.example.androiddevchallenge.R
@@ -24,30 +8,45 @@ val catList = listOf(
         image = R.drawable.black_cat,
         name = "Pretinho",
         description = "Hate dogs chase imaginary bugs see owner, run in terror\n" +
-            "Sleeps on my head time to go zooooom. Chew the plant reward the chosen human with a slow blink purrrrrr, or oooo! dangly balls!"
+            "Sleeps on my head time to go zooooom. Chew the plant reward the chosen human with a " +
+            "slow blink purrrrrr, or oooo! dangly balls!"
     ),
     Cat(
         id = 1,
         image = R.drawable.grey_cat,
         name = "Cinzinha",
-        description = "Scratch me there, elevator butt commence midnight zoomies why use post when this sofa is here get scared by sudden appearance of cucumber and my water bowl is clean and freshly replenished, so i'll drink from the toilet massacre a bird in the living room and then look like the cutest and most innocent animal on the planet."
+        description = "Scratch me there, elevator butt commence " +
+            "midnight zoomies why use post when this sofa is here get" +
+            " scared by sudden appearance of cucumber and my water " +
+            "bowl is clean and freshly replenished, so i'll drink" +
+            " from the toilet massacre a bird in the living room and then" +
+            " look like the cutest and most innocent animal on the planet."
     ),
     Cat(
         id = 2,
         image = R.drawable.grey_cat_2,
         name = "Rajadinho",
-        description = "Rub whiskers on bare skin act innocent purr when give birth yet destroy couch, for eat an easter feather as if it were a bird then burp victoriously, but tender. Stuff and things. Under the bed sleep over your phone and make cute snoring noises."
+        description = "Rub whiskers on bare skin act i" +
+            "nnocent purr when give birth yet destroy couch, for eat " +
+            "an easter feather as if it were a bird then burp victoriously, but tender." +
+            " Stuff and things. Under the bed sleep over your phone and make cute snoring noises."
     ),
     Cat(
         id = 3,
         image = R.drawable.siames_cat,
         name = "Siamezinho",
-        description = "Reward the chosen human with a slow blink cat snacks spend six hours per day washing, but still have a crusty butthole cough but sit on the laptop and murr i hate humans they are so annoying."
+        description = "Reward the chosen human with a slow blink cat snacks " +
+            "spend six hours per day washing, but still have a crusty butthole cough but " +
+            "sit on the laptop and murr i hate humans they are so annoying."
     ),
     Cat(
         id = 4,
         image = R.drawable.yellow_cat,
         name = "Amarelinho",
-        description = "Meow meow. Paw your face to wake you up in the morning paw at your fat belly stinky cat. Leave fur on owners clothes i shredded your linens for you so slap the dog because cats rule yet bird bird bird bird bird bird human why take bird out i could have eaten that for get my claw stuck in the dog's ear love for sleep on my human's head. "
-    ),
+        description = "Meow meow. Paw your face to wake you up in the morning paw " +
+            "at your fat belly stinky cat. Leave fur on owners clothes i shredded " +
+            "your linens for you so slap the dog because cats rule yet bird bird bird " +
+            "bird bird bird human why take bird out i could have eaten that for get my" +
+            " claw stuck in the dog's ear love for sleep on my human's head. "
+    )
 )
